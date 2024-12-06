9933d1ae6a248bf9d094ba3640b35876c36a0646
keemjoonsung <kjs990114@naver.com>
2024-12-06 19:51:37 +0900

    Initial commit

---
+++ src/main/kotlin/com/kjs990114/tradesimapi/TradesimApiApplication.kt
@@ -0,0 +1,11 @@
+package com.kjs990114.tradesimapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TradesimApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TradesimApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/kjs990114/tradesimapi/TradesimApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kjs990114.tradesimapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TradesimApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
