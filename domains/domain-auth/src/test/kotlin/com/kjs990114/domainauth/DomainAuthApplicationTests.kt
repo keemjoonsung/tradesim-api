3c14dfb143eb0e5b4ad9a2e3ebac10f87453a282
keemjoonsung <kjs990114@naver.com>
2024-12-16 23:34:02 +0900

    Multi Module settings

---
+++ domains/domain-auth/src/test/kotlin/com/kjs990114/domainauth/DomainAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kjs990114.domainauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DomainAuthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,7 @@ include(
     "server-trade-api",
     "server-trade-ws",
     "domain-trade",
+    "domain-auth",
     "app-trade",
     "app-auth",
     "infra-trade"
@@ -12,6 +13,7 @@ include(
 project(":server-trade-api").projectDir = file("servers/server-trade-api")
 project(":server-trade-ws").projectDir = file("servers/server-trade-ws")
 project(":domain-trade").projectDir = file("domains/domain-trade")
+project(":domain-auth").projectDir = file("domains/domain-auth")
 project(":app-trade").projectDir = file("apps/app-trade")
 project(":app-auth").projectDir = file("apps/app-auth")
 project(":infra-trade").projectDir = file("infrastructures/infra-trade")
